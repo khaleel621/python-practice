e9beafb770aadc52c2d0146974dc9a5b4e328af2
khaleel621 <khaleel18181818@gmail.com>
2026-05-20 16:33:07 +0530

    Create day3_practice.py

---
+++ day3_practice.py
@@ -0,0 +1,9 @@
+# Day 3 - Type Casting
+
+age = int("21")        # String → int
+price = int(3.99)      # Float → int (becomes 3)
+marks = float("95.5")  # String → float
+label = str(88)        # int → String
+
+num = int(input("Enter a number: "))
+print("Double:", num * 2)
